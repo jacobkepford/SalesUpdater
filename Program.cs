84589f411f6e842e3a571176b2ac6f579d477d37
Jacob <jacobkepford@gmail.com>
2021-03-21 12:19:03 -0500

    moved service to new class

--- Program.cs
+++ Program.cs
@@ -11,43 +11,20 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace GmailQuickstart
+namespace SalesUpdater
 {
     class Program
     {
-        // If modifying these scopes, delete your previously saved credentials
-        // at ~/.credentials/gmail-dotnet-quickstart.json
-        static string[] Scopes = { GmailService.Scope.GmailReadonly };
-        static string ApplicationName = "Gmail API .NET Quickstart";
 
         static void Main(string[] args)
         {
             UserCredential credential;
+            
+            RegisterService service = new RegisterService();
+            credential = service.GetCredentials();
+            GmailService running_service = service.StartService(credential);
 
-            using (var stream =
-                new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
-            {
-                // The file token.json stores the user's access and refresh tokens, and is created
-                // automatically when the authorization flow completes for the first time.
-                string credPath = "token.json";
-                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
-                    GoogleClientSecrets.Load(stream).Secrets,
-                    Scopes,
-                    "user",
-                    CancellationToken.None,
-                    new FileDataStore(credPath, true)).Result;
-                Console.WriteLine("Credential file saved to: " + credPath);
-            }
-
-            // Create Gmail API service.
-            var service = new GmailService(new BaseClientService.Initializer()
-            {
-                HttpClientInitializer = credential,
-                ApplicationName = ApplicationName,
-            });
-
-            // Define parameters of request.
-            UsersResource.LabelsResource.ListRequest request = service.Users.Labels.List("me");
+            UsersResource.LabelsResource.ListRequest request = running_service.Users.Labels.List("rtbredge@gmail.com");
 
             // List labels.
             IList<Label> labels = request.Execute().Labels;
---
+++ RegisterService.cs
@@ -0,0 +1,54 @@
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SalesUpdater
+{
+    public class RegisterService : GmailService
+    {
+        static string[] Scopes = { GmailService.Scope.GmailReadonly };
+        new static string ApplicationName = "Gmail API .NET Quickstart";
+
+        UserCredential credential;
+
+        public UserCredential GetCredentials(){
+            using (var stream =
+                new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+            return credential;
+
+        }
+        public GmailService StartService(UserCredential credential){
+            
+            // Create Gmail API service.
+            var service = new GmailService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            return service;
+        }
+        
+    }
+}
