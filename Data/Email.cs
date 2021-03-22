15cef4ec1a59bec8e485e9cd211530cb1ec3f70b
Jacob <jacobkepford@gmail.com>
2021-03-22 17:01:14 -0500

    Added a new Email class

---
+++ Data/Email.cs
@@ -0,0 +1,10 @@
+namespace SalesUpdater.Data
+{
+    public class Email
+    {
+        public int OrderNumber { get; set; }
+        public string Product { get; set; }
+        public int Quantity { get; set; }
+        public string OrderPerson { get; set; }
+    }
+}
