e3994f9e864e1a872d212ed747a4e31a0d5d5e4e
Jacob <jacobkepford@gmail.com>
2021-03-21 20:09:09 -0500

    Upgraded to dot net 3.1

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SalesUpdater
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //Create a new Gmail API Service
+            EmailService service = new EmailService();
+            GmailService active_service = service.NewService();
+
+            //Make a request to pull emails
+            UsersResource.MessagesResource.ListRequest emailListRequest = active_service.Users.Messages.List("me");
+            emailListRequest.LabelIds = "Label_6420272116865146";
+
+
+            // List message.
+            List<Message> messageDataItems = new List<Message>();
+            IList<Message> messages = emailListRequest.Execute().Messages;
+            if (messages != null && messages.Count > 0)
+            {
+                foreach (var message in messages)
+                {
+                    var emailInfoRequest = active_service.Users.Messages.Get("me", message.Id);
+                    Message emailData = emailInfoRequest.Execute();
+                    messageDataItems.Add(emailData);
+                }
+                foreach (var messageItem in messageDataItems)
+                {
+                    string body = messageItem.Payload.Parts[0].Body.Data;
+                    String codedBody = body.Replace("-", "+");
+                    codedBody = codedBody.Replace("_", "/");
+                    byte[] data = Convert.FromBase64String(codedBody);
+                    body = Encoding.UTF8.GetString(data);
+                    Console.WriteLine(body);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No messages found.");
+            }
+        }
+    }
+}
