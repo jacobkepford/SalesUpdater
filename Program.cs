d92a534a893b421beb68f342f52fa0f959df0877
Jacob <jacobkepford@gmail.com>
2021-03-22 13:36:43 -0500

    removed Email Service class and moved all to program.cs

--- Program.cs
+++ Program.cs
@@ -18,27 +18,26 @@ namespace SalesUpdater
 
         static void Main(string[] args)
         {
+            string[] Scopes = { GmailService.Scope.GmailReadonly };
+            string ApplicationName = "SalesUpdater";
+
+            UserCredential credential = GetCredentials(Scopes);
+
             //Create a new Gmail API Service
-            EmailService service = new EmailService();
-            GmailService active_service = service.NewService();
-
-            //Make a request to pull emails
-            UsersResource.MessagesResource.ListRequest emailListRequest = active_service.Users.Messages.List("me");
-            emailListRequest.LabelIds = "Label_6420272116865146";
-
-
-            // List message.
-            List<Message> messageDataItems = new List<Message>();
-            IList<Message> messages = emailListRequest.Execute().Messages;
-            if (messages != null && messages.Count > 0)
+            var service = new GmailService(new BaseClientService.Initializer()
             {
-                foreach (var message in messages)
-                {
-                    var emailInfoRequest = active_service.Users.Messages.Get("me", message.Id);
-                    Message emailData = emailInfoRequest.Execute();
-                    messageDataItems.Add(emailData);
-                }
-                foreach (var messageItem in messageDataItems)
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+            
+            
+            //Make a request to pull emails
+            UsersResource.MessagesResource.ListRequest emailListRequest = service.Users.Messages.List("me");
+            emailListRequest.LabelIds = "Label_6420272116865146";
+            
+            List<Message> messageDataItems = GetEmails(emailListRequest, service);
+
+            foreach (var messageItem in messageDataItems)
                 {
                     string body = messageItem.Payload.Parts[0].Body.Data;
                     String codedBody = body.Replace("-", "+");
@@ -47,11 +46,54 @@ namespace SalesUpdater
                     body = Encoding.UTF8.GetString(data);
                     Console.WriteLine(body);
                 }
+
+            
+        }
+        static UserCredential GetCredentials(string[] scopes)
+        {
+            UserCredential credential;
+            using (var stream =
+                new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+            return credential;
+
+        }
+
+        static List<Message> GetEmails(UsersResource.MessagesResource.ListRequest emailListRequest, GmailService service)
+        {
+        // List message.
+            List<Message> messageDataItems = new List<Message>();
+            IList<Message> messages = emailListRequest.Execute().Messages;
+            if (messages != null && messages.Count > 0)
+            {
+                foreach (var message in messages)
+                {
+                    var emailInfoRequest = service.Users.Messages.Get("me", message.Id);
+                    Message emailData = emailInfoRequest.Execute();
+                    messageDataItems.Add(emailData);
+                }
+
+                return messageDataItems;
+                
             }
             else
             {
                 Console.WriteLine("No messages found.");
+                return messageDataItems;
             }
+
         }
+
     }
 }
