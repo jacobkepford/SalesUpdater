46aa587d07a000e9c07fe40317e3ac0c6231eb2a
Jacob <jacobkepford@gmail.com>
2021-03-24 15:26:22 -0500

    Continuing to update solution to use new Email API project

--- EmailApi/EmailService.cs
+++ EmailApi/EmailService.cs
@@ -11,12 +11,14 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
+using EmailApi.Data;
 
 namespace EmailApi
 {
     public class EmailService
     {
-        public GmailService EmailApiService { get; set; }
+        public GmailService Connection { get; set; }
+        public string Label { get; set; }
 
         public EmailService()
         {
@@ -25,12 +27,34 @@ namespace EmailApi
             
             UserCredential credential = GetCredentials(scopes);
             
-            EmailApiService = new GmailService(new BaseClientService.Initializer()
+            Connection = new GmailService(new BaseClientService.Initializer()
         {
             HttpClientInitializer = credential,
             ApplicationName = applicationName,
         });
         
+        }
+
+
+        private static UserCredential GetCredentials(string[] scopes)
+        {
+            UserCredential credential;
+            using (var stream =
+                new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+            return credential;
+
         }
 
         // API Request to get Email and Email Metadata
@@ -59,40 +83,60 @@ namespace EmailApi
             return messageDataItems;
 
         }
+
+        //Pull email body from each email supplied
+        public List<string> GetMessageBodies(List<Message> messageDataItems)
+        {
+            List<string> messageBodies = new List<string>();
+
+            foreach (var messageItem in messageDataItems)
+            {
+                string body = messageItem.Payload.Parts[0].Body.Data;
+                string codedBody = body.Replace("-", "+");
+                codedBody = codedBody.Replace("_", "/");
+                byte[] data = Convert.FromBase64String(codedBody);
+                body = Encoding.UTF8.GetString(data);
+                body = body.Replace("\r\n", "");
+                messageBodies.Add(body);
+            }
+            return messageBodies;
+        }
+
+        public List<Email> ExtractEmailData(List<string> messageBodies)
+        {
+            List<Email> emails = new List<Email>();
+            
+            foreach (var message in messageBodies)
+            {
+                Email email = new Email();
+
+                //Format and run regex search for Order ID
+                string orderIDExpr = "Order: \\#([0-9]+)";
+                email.OrderNumber = ShowMatch(message, orderIDExpr);
+
+                //Format and run regex search for product
+                string orderProductExpr = "Price(.*) [0-9] \\$";
+                email.Product = ShowMatch(message, orderProductExpr);
+                Console.WriteLine(email.Product);
+
+                emails.Add(email);
+
+            }
+
+            return emails;
+        }
+
+        private static string ShowMatch(string text, string expr)
+        {
+            Match m = Regex.Match(text, expr);
+            Group g = m.Groups[1];
+            return g.ToString();
+
+        }
         
-        // //Format a request to pull email id's
-        // UsersResource.MessagesResource.ListRequest emailListRequest = service.Users.Messages.List("me");
-        // //LabelID = Orders
-        // emailListRequest.LabelIds = "Label_6420272116865146";
-
-        // //Execute Email request to get all email metadata
-        // List<Message> messageDataItems = GetEmails(service, emailListRequest);
-
-        // //Extract email body from each email
-        // List<string> messageBodies = GetMessageBodies(messageDataItems);
-
         // // ExtractEmailData(messageBodies);
 
         //Get the user credentials stored in credentials.json
-        private static UserCredential GetCredentials(string[] scopes)
-        {
-            UserCredential credential;
-            using (var stream =
-                new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
-            {
-                // The file token.json stores the user's access and refresh tokens, and is created
-                // automatically when the authorization flow completes for the first time.
-                string credPath = "token.json";
-                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
-                    GoogleClientSecrets.Load(stream).Secrets,
-                    scopes,
-                    "user",
-                    CancellationToken.None,
-                    new FileDataStore(credPath, true)).Result;
-                Console.WriteLine("Credential file saved to: " + credPath);
-            }
-            return credential;
-
-        }
+        
     }
 }
