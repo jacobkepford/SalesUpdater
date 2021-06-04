2a348b98a07c41059b21840666e664355cc0f330
Jacob <jacobkepford@gmail.com>
2021-06-04 13:32:19 -0500

    Adding more test patterns to testing project

--- SalesTests/UtilitiesTest.cs
+++ SalesTests/UtilitiesTest.cs
@@ -6,14 +6,28 @@ namespace SalesTests
 {
     public class UtilitiesTest
     {
-        [Fact]
-        public void EmailSearchMatch()
-        {
-            string message = "New Order: #32258You’ve received the following order from Richard Seger:[Order #32258]<http://track.smtpsendemail.com/9037916/c?p=F_sOud9wG23ZzA0j8Ylq_BB7vhhFf4-XzrWl_278dqa6-o97mphUPLZ808GS0mdeWYKaTOoY6xMvW7rtFS-AOLMn2pwnwfVZziKYHrPmnsoXTG3DXFtTKH-LvlwPMA-LtxVzPrBqvvwUJ05yH8gQqkkLdMPaLJ6ydbHmpwLZrFoA4spVyBvipc-3wIx0cZe->(March20, 2021)ProductQuantityPriceTractor Loader Quick Attach Replacement Kubota LA950A, LA1001, LA1002,LA1150A, BF800, BF900, BF1100, LA950, LA1100 & LA1150 (#QA-KU5) 1 $899.00Subtotal: $899.00Discount: -$20.21Shipping: $199.97 via Shipping & HandlingTax: $0.00Payment method: Credit CardTotal: $1,078.76Order Notes   - There are no notes for this order yet.Billing addressRichard Seger62910 County Line RDThree Rivers, MI 49093269-816-2559 <2698162559>pnr149@yahoo.com Shipping addressRichard Seger62910 County Line RDThree Rivers, MI 49093";
-            string orderPersonNameExpr = "order from ([a-zA-z]* [a-zA-Z?][a-zA-z]*)";
-            string orderPerson = EmailUtilities.EmailSearch(message, orderPersonNameExpr);
+        public string Message { get; set; }
 
-            Assert.Equal("Richard Seger", orderPerson);
+        public UtilitiesTest()
+        {
+            Message = "New Order: #32258You’ve received the following order from Richard Seger:[Order #32258]<http://track.smtpsendemail.com/9037916/c?p=F_sOud9wG23ZzA0j8Ylq_BB7vhhFf4-XzrWl_278dqa6-o97mphUPLZ808GS0mdeWYKaTOoY6xMvW7rtFS-AOLMn2pwnwfVZziKYHrPmnsoXTG3DXFtTKH-LvlwPMA-LtxVzPrBqvvwUJ05yH8gQqkkLdMPaLJ6ydbHmpwLZrFoA4spVyBvipc-3wIx0cZe->(March20, 2021)ProductQuantityPriceTractor Loader Quick Attach Replacement Kubota LA950A, LA1001, LA1002,LA1150A, BF800, BF900, BF1100, LA950, LA1100 & LA1150 (#QA-KU5) 1 $899.00Subtotal: $899.00Discount: -$20.21Shipping: $199.97 via Shipping & HandlingTax: $0.00Payment method: Credit CardTotal: $1,078.76Order Notes   - There are no notes for this order yet.Billing addressRichard Seger62910 County Line RDThree Rivers, MI 49093269-816-2559 <2698162559>pnr149@yahoo.com Shipping addressRichard Seger62910 County Line RDThree Rivers, MI 49093";
+        }
+        [Theory]
+        //Order Number
+        [InlineData("Order: \\#([0-9]+)", "32258")]
+        //Product
+        [InlineData("Price(.*) [\\d]* \\$", "Tractor Loader Quick Attach Replacement Kubota LA950A, LA1001, LA1002,LA1150A, BF800, BF900, BF1100, LA950, LA1100 & LA1150 (#QA-KU5)")]
+        //Quantity
+        [InlineData("Price.* ([\\d]*) \\$", "1")]
+        //Name
+        [InlineData("order from ([a-zA-z]* [a-zA-Z?][a-zA-z]*):", "Richard Seger")]
+        //Order Date
+        [InlineData("\\(([A-Z][a-z]+[0-9]*, [0-9]{4})\\)Product", "March20, 2021")]
+        public void EmailSearchMatch(string expr, string result)
+        {
+            string message = Message;
+            string orderPerson = EmailUtilities.EmailSearch(message, expr);
+            Assert.Equal(result, orderPerson);
         }
 
     }
